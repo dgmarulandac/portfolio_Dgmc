f93f19b8711316edda14b81d1bff9cbb9847fdd6
dgmarulandac <54507534+dgmarulandac@users.noreply.github.com>
2023-10-07 22:36:39 -0500

    update variable img

--- src/components/Work.js
+++ src/components/Work.js
@@ -63,7 +63,7 @@ const Work = () => {
             <div className='group-hover:bg-sky-300/70 w-full h-full absolute z-40 transition-all duration-300'>
 
             </div>
-            <img className="group-hover:scale-125 transition-all duration-500"src={img1} alt="img1"/>
+            <img className="group-hover:scale-125 transition-all duration-500"src={img2} alt="img1"/>
             <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 font-bold text-xl'>
               <span>UI/UX design</span>
             </div>
@@ -77,7 +77,7 @@ const Work = () => {
             <div className='group-hover:bg-sky-300/70 w-full h-full absolute z-40 transition-all duration-300'>
 
             </div>
-            <img className="group-hover:scale-125 transition-all duration-500"src={img1} alt="img1"/>
+            <img className="group-hover:scale-125 transition-all duration-500"src={img3} alt="img1"/>
             <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 font-bold text-xl'>
               <span>UI/UX design</span>
             </div>
